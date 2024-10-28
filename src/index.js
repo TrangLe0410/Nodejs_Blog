19642e1a2b4312952bcf399b29f0249fc2f15f0f
TrangLe <letrangle2002@gmail.com>
2024-10-28 09:16:56 +0700

    scss and bootrap

---
+++ src/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const morgan = require('morgan');
+const exphbs = require('express-handlebars');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(morgan('combined'));
+
+const hbs = exphbs.create({
+    extname: '.hbs'
+});
+app.engine('hbs', hbs.engine);
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources\\views'));
+
+app.get('/', (req, res) => {
+    res.render('home');
+})
+
+app.get('/news', (req, res) => {
+    res.render('news');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
